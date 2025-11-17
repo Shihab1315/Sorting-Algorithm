a56481a4ec927106732fa1eaeb95d79a99a9c518
Md Shihab uddin <166051075+Shihab1315@users.noreply.github.com>
2025-11-17 16:43:37 +0600

    Add files via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool ft = false;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+                ft = true;
+            }
+        }
+        if (ft == false)
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
